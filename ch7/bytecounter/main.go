1cbc6200281724ff5e2b24fb03034e89c0eb22f2
Ferdinand Enario <ftenario@@gmail.com>
2020-11-12 07:49:59 -0800

    Add ch7/bytecounter

---
+++ ch7/bytecounter/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type ByteCounter int
+
+func (b *ByteCounter) Write(p []byte) (int, error) {
+	*b += ByteCounter(len(p))
+	return len(p), nil
+}
+
+func main() {
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c)
+
+	c = 0
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c)
+	fmt.Println(&c)
+}
